e0c165794db4fee6b935d0c1674ed20e2689b258
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-03-29 13:55:16 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+               int N,i,j,t;
+               string s ;
+               cout<<"inserisci il numero di studenti"<<endl;
+               cin>> N;
+               vector<string>nomi(N);
+               vector<float>voti(N);
+               i=0;
+               while(i<N)
+               {
+                  cout<<"inserisci il nome dello studente"<<endl;
+                  cin>> nomi(i);
+                  cout<<"inserisci il voto dello studente"<<endl;
+                  cin>> voti(i);
+                  i=i+1;             
+               }
+            i=0
+            while(i<N)
+            {
+                j=0;
+                while(j<(N-1)-i)
+                {
+                    if(voti(j)>voti(j+1))
+                    {
+                        t=voti(j);
+                        voti(j)=voti(j+1);
+                        voti(j+1)=t;
+                        s=nomi(j);
+                        nomi(j)=nomi(j+1);
+                        nomi(j+1)=s;
+                    }
+                j=j+1;
+                }
+            i=i+1;
+            }
+        i=0;
+        while(i<N)
+        {
+            cout<< nomi(i);
+            cout<< voti(i);
+            i=i+1;
+        }
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
